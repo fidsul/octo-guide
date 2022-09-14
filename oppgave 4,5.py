30cdead2d809b8323da4a9bc9b887502067f29a6
fidsul <101518756+fidsul@users.noreply.github.com>
2022-09-14 13:53:02 +0200

    har gjort oppgaver

---
+++ oppgave 4,5.py
@@ -0,0 +1,5 @@
+NAVN = input("skriv navnet ditt her: (f.eks kevin)")
+XX = input("skriv høyden din her: (f.eks 1.85)")
+YY = input("skriv inn din alder: (f.eks 16)")
+
+print(f"Du heter {NAVN}, du er {XX} meter høy og {YY} år gammel. Gratulerer!"  )
---
+++ oppgave 4,6.py
@@ -0,0 +1,5 @@
+while True:
+    num_1 = eval(input("Skriv inn et tall:"))
+    num_2 = eval(input("Skriv inn et tall:"))
+
+    print(f"{num_1} + {num_2} = {num_1 + num_2}")
